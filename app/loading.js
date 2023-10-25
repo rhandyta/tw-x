a9145e6672735928b84a605de44afd9c5c573080
rhandyta <rhandytabriantama@gmail.com>
2023-10-25 23:31:41 +0700

    loading

--- app/loading.js
+++ app/loading.js
@@ -1,29 +1,30 @@
-import { Box } from "@mui/material"
-import LoadingProgress from "./components/LoadingProgress"
-
+import { Box } from "@mui/material";
+import LoadingProgress from "./components/LoadingProgress";
 
 function loading() {
   return (
-    <Box sx={{
-      position: "relative",
-      width: "100%",
-      height: "100vh"
-    }}>
-      <Box sx={{
-        position: "absolute",
-        width: "100%",
-        height: "100%",
-        top: "80%",
-        left: "90%",
-        zIndex: 1000,
-        transform: `translate(-50%, -50%)`
-      }}>
-
-      <LoadingProgress />
-
-      </Box>
-    </Box>
-  )
+    <>
+        <Box
+          sx={{
+            position: "relative",
+          }}
+        >
+          <Box
+            sx={{
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+              top: "80%",
+              left: "90%",
+              zIndex: 1000,
+              transform: `translate(-50%, -50%)`,
+            }}
+          >
+            <LoadingProgress />
+          </Box>
+        </Box>
+    </>
+  );
 }
 
-export default loading
+export default loading;
