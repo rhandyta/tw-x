958fbb25cd2937dbdd7a73e8b32c381b3dec6988
rhandyta <rhandytabriantama@gmail.com>
2023-10-26 23:31:03 +0700

    logout

--- components/User.js
+++ components/User.js
@@ -5,14 +5,30 @@ import Link from "next/link";
 import CustomButton from "@/components/CustomButton";
 import SignIn from "@/components/pages/contact/SignIn";
 import { deleteUser } from "firebase/auth";
+import { Stack } from "@mui/material";
+import { useRouter } from "next/navigation";
 
 function User() {
+    
+    const router = useRouter()
+
+    function logoutHandler() {
+        deleteUser(auth.currentUser).then(() => {
+            router.refresh()
+          }).catch((error) => {
+            // 
+          });
+    }
+
   return (
     <>
       {auth.currentUser ? (
-        <Link href="/contact" className="button-link">
-          <CustomButton>Send Message</CustomButton>
-        </Link>
+        <Stack justifyContent="center" width="100%" rowGap={2}>
+          <Link href="/contact" className="button-link">
+            <CustomButton>Send Message</CustomButton>
+          </Link>
+          <CustomButton onClick={logoutHandler}>Logout</CustomButton>
+        </Stack>
       ) : (
         <SignIn />
       )}
