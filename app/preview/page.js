b611d2fa22baaee733d73342c467d15c79081256
rhandyta <rhandytabriantama@gmail.com>
2023-11-10 23:08:38 +0700

    cv

--- app/preview/page.js
+++ app/preview/page.js
@@ -3,12 +3,12 @@ import React from "react";
 function page() {
   return (
     <iframe
-      src="https://drive.google.com/file/d/1hBGMjj4onYUnucgg20y7l9xbsPvVuApD/preview"
+      src="https://drive.google.com/file/u/1/d/161ZSciYQaRjSJfJZ7vBNzZEdLD4qF7rA/preview"
       frameBorder="0"
       scrolling="auto"
       width="100%"
       style={{
-        height: "80vh"
+        height: "90vh"
       }}
     ></iframe>
   );
