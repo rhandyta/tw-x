d969adc4f69721c679f9492132954ba6c7ab7052
rhandyta <rhandytabriantama@gmail.com>
2023-11-17 06:55:49 +0700

    Manifest

---
+++ app/manifest.js
@@ -0,0 +1,28 @@
+export default function manifest() {
+    return {
+        "name": "Portfolio Rhandyta Briantama",
+        "short_name": "Rhand",
+        "description": "Web Profile Portfolio Rhandyta Briantama sebagai Software Engineer.",
+        "start_url": "/",
+        "display": "standalone",
+        "background_color": "#ffffff",
+        "theme_color": "#4285f4",
+        "lang": "id-ID",
+        "orientation": "portrait",
+        "scope": "/",
+        "prefer_related_applications": false,
+        "icons": [
+          {
+            "src": "android-chrome-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "android-chrome-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ]
+      }
+      
+}
